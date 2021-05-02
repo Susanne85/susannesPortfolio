87a649b0ee0444e74a833f174c042b2d37c076f1
Susanne Bilney <xkqk2010@gmail.com>
2021-05-02 15:27:27 +0000

    Hamburger Menu operates correctly on mobile devices

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,23 @@ heroImg.addEventListener('mouseleave', function(event){
     event.currentTarget.classList.remove('old-pic');
 
 });
+
+const burgerIcon = document.querySelector('.navbar-burger');
+const burgerMenu = document.querySelector('#navbarPortfolio');
+const burgerDropDown = document.querySelector('.navbar-link');
+
+burgerIcon.addEventListener('click', function (event) {
+  burgerMenu.classList.toggle('is-active');
+  const burgerItemDropDown = document.querySelector('.navbar-dropdown');
+  burgerItemDropDown.style.display = 'none';
+});
+burgerDropDown.addEventListener('click', function (event) {
+  const burgerItemDropDown = document.querySelector('.navbar-dropdown');
+
+  if (burgerItemDropDown.style.display === 'none') {
+    burgerItemDropDown.style.display = 'block';
+  }
+  else {
+    burgerItemDropDown.style.display = 'none';
+  }
+});
